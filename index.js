7d159b9c0c88e7e18f8cd3d917c7e55220d3a095
Hany23-code <hanypatel2310@gmail.com>
2024-09-16 14:03:36 +1000

    added live time updated functions

--- index.js
+++ index.js
@@ -1,26 +1,20 @@
-function cityFn(event) {
+function searchInput(event) {
   event.preventDefault();
-  let city = document.querySelector("#cityName");
-  let valueCity = city.value;
-
-  let h1 = document.querySelector("h1");
-  h1.innerHTML = `${valueCity}`;
+  let cityInput = document.querySelector("#cityName");
+  inputFn(cityInput.value);
 }
-let form = document.querySelector("#city-input");
-form.addEventListener("submit", cityFn);
+let form = document.querySelector("#city-form");
+form.addEventListener("submit", searchInput);
 
-document.querySelector("#city-input").addEventListener("submit", inputFn);
-function inputFn(event) {
-  event.preventDefault();
-
-  let query = document.querySelector("#cityName").value; // Get the input value
+function inputFn(city) {
   let apiKey = "694b5c6135cad6febfb0969t43a765co";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${query}&key=${apiKey}&units=metric`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 
   axios.get(apiUrl).then(weatherFn);
 }
 function weatherFn(response) {
   console.log(response.data);
+  let city = document.querySelector("#city");
   let temperature = document.querySelector("#currentTemp");
   let daytime = document.querySelector("#day-time");
   let currentHumid = document.querySelector("#humid");
@@ -30,6 +24,7 @@ function weatherFn(response) {
   let date = new Date(response.data.time * 1000);
   let time = document.querySelector("#day-time");
 
+  city.innerHTML = response.data.city;
   time.innerHTML = dayTime(date);
   temperature.innerHTML = Math.round(response.data.temperature.current);
   currentHumid.innerHTML = response.data.temperature.humidity;
@@ -64,3 +59,4 @@ function dayTime(date) {
   let day = days[date.getDay()];
   return `${day} ${hour}:${minute}`;
 }
+inputFn("canberra");
